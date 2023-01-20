7ba3951100d8920612fa1fa6fca25e32878c510c
Unai Zuluaga <zuluagaunai@gmail.com>
2023-01-20 09:24:49 +0100

    Atributes of the Entities changed to Properties + Controller started

--- src/application/G4AetherClientSide.java
+++ src/application/G4AetherClientSide.java
@@ -5,13 +5,21 @@
  */
 package application;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import view.logIn.LogInController;
+import view.viewDiagnosis.DiagnosisController;
 
 /**
  *
@@ -20,25 +28,27 @@ import javafx.stage.Stage;
 public class G4AetherClientSide extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage stage) throws Exception{
+   FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Diagnosis/Diagnosis.fxml"));
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+      
         
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Parent root = (Parent) loader.load();
+      
+              DiagnosisController controller = (DiagnosisController) loader.getController();
+
+      controller.setStage(stage);
+
+        controller.initialize(root);
+
+        Scene scene = new Scene(root);
+
+        stage.setResizable(false);
+        stage.setTitle("LogIn");
+        stage.getIcons().add(new Image("/resources/icon.png"));
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     /**
--- src/view/viewDiagnosis/DiagnosisController.java
+++ src/view/viewDiagnosis/DiagnosisController.java
@@ -6,22 +6,28 @@
 package view.viewDiagnosis;
 
 import entities.Diagnosis;
+import static javafx.application.Application.launch;
 import javafx.beans.property.SimpleObjectProperty;
 
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
 
-public class DiagnosisController {
-
+public class DiagnosisController  {
+public Stage stage;
     @FXML
     private VBox vBox;
 
@@ -93,7 +99,31 @@ public class DiagnosisController {
 
     @FXML
     private Button btnPrint;
+    
+   public void initialize(Parent root) {
+
+        Scene scene = new Scene(root);
+
+        
+        //Not a resizable window.
+        stage.setResizable(false);
+        //Modal window of LogIn.
+        stage.initModality(Modality.APPLICATION_MODAL);
+        //The window title will be ”SignUp”
+        stage.setTitle("Controller");
+        //Add a leaf icon.
+        stage.getIcons().add(new Image("resources/icon.png"));
+        //Add scene
+        stage.setScene(scene);
+        //Show window
+        stage.show();
+   }
+   
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
 
 }
+ 
 
-
+ 
